784500bc6cf4edf62cc06d4b7a3baf5dc7986f6f
Pierre Payet <pierre.p@synergies-france.fr>
2021-08-03 10:13:32 +0200

    add custom package

--- src/polyfills/index.ts
+++
@@ -1,9 +0,0 @@
-/*eslint-disable */
-
-import 'core-js/fn/array/includes'
-import 'core-js/fn/object/assign'
-import 'core-js/fn/object/entries'
-import 'core-js/fn/object/values'
-import 'core-js/fn/string/starts-with'
-import 'core-js/fn/array/from'
-import 'core-js/fn/array/find'
--- src/types/vuex.ts
+++
@@ -1,7 +0,0 @@
-import Database from '../database/Database'
-
-declare module 'vuex' {
-  interface Store<S> {
-    $db(): Database
-  }
-}
